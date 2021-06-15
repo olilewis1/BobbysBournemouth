54fd26ba72639561c6acde5426b5300f9f24b73b
olilewis1 <olilewis1@hotmail.co.uk>
2021-06-15 19:21:35 +0100

    added parts to homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,39 @@
-import React, { useState } from 'react'
-import bobbysPicOld from '../assets/Screenshot 2021-06-03 at 19.24.38.png'
-import stuartSemple from '../assets/close up front v5.jpg'
-import { Carousel } from 'react-responsive-carousel'
+import React from 'react'
+// import bobbysPicOld from '../assets/Screenshot 2021-06-03 at 19.24.38.png'
+import stuartSemple from '../assets/close up front v8.jpg'
+// import { Carousel } from 'react-responsive-carousel'
 const Home = () => {
-  const [intervalz, setIntervalz] = useState(3000) //initial state here represents the interval for first image.
+  // const [intervalz, setIntervalz] = useState(3000) //initial state here represents the interval for first image.
 
-  const onChange = (index, item) => {
-    setIntervalz(item.props['data-interval'])
-  }
+  // const onChange = (index, item) => {
+  //   setIntervalz(item.props['data-interval'])
+  // }
 
   return (
     // 
     //   <img src={bobbysPicOld} alt="" /> 
     // 
-    <section className="hero is-medium">
-      <Carousel
+    <section className="hero ">
+      {/* <Carousel
         onChange={onChange}
         autoPlay
         interval={intervalz}
-        infiniteLoop={false}>
-        <div data-interval={3000}>
+        infiniteLoop={false}> */}
+      {/* <div data-interval={3000}>
           <img className="image-height" src={bobbysPicOld} alt="Coming Soon" />
           <p className="legend">Legend 1</p>
-        </div>
+        </div> */}
 
-        <div data-interval={3000}>
-          <img className="image-height" src={stuartSemple} alt="Coming Soon" />
-          <p className="legend">Legend 3</p>
-        </div>
-
-      </Carousel>
+      <div data-interval={3000}>
+        <img className="image-height" src={stuartSemple} alt="Coming Soon" />
+        <p className="legend">Legend 3</p>
+      </div>
+      <div className="columns is-vcentered has-text-centered	is-hidden-tablet	 ">
+        {/* <div className="column"></div> */}<div className=""> </div>
+        <h1 className="center column ml-2 is-size-6-mobile 	">Coming Soon.
+        </h1>
+      </div>
+      {/* </Carousel> */}
     </section>
   )
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,21 +7,21 @@ import instagram from '../assets/instagram_icon_138461.png'
 const Navbar = () => {
   return (
 
-    <nav className="navbar is-white has-text-grey		">
-      <div className="container">
+    <nav className="navbar is-white	">
+      <div className="container is-flex is-justify-content-space-between	">
         <div className="navbar-start ">
-          <div className="navbar-brand image is-128x128">
+          <div className="navbar-brand image is-128x128 is-64x64">
             <Link to="/"> <img src={bobbysButton} alt="" />
             </Link>
           </div>
 
         </div>
-        <div className="columns is-vcentered">
+        <div className="columns is-vcentered is-hidden-mobile">
           {/* <div className="column"></div> */}<div className=""> </div>
-          <h1 className="center column ml-2">Relaunch Coming Soon.
+          <h1 className="center column ml-2 is-size-6-mobile	">Coming Soon.
           </h1>
         </div>
-        <div className="navbar-end is-hidden-touch">
+        <div className="navbar-end is-flex ">
           <a href="https://www.facebook.com/bobbysbournemouth/"><img className="mt-5 is-centered is-64x64 image" src={facebook} alt="" /></a>
           <a href="https://www.instagram.com/bobbysbournemouth/"><img className="mt-5 is-centered is-64x64 image" src={instagram} alt="" /></a>
         </div>
