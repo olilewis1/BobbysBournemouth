093d38f6def1d16b7b22c732167b16f3baf193a5
olilewis1 <olilewis1@hotmail.co.uk>
2021-09-06 16:27:01 +0100

    changed text

--- src/components/Home.js
+++ src/components/Home.js
@@ -104,9 +104,6 @@ const Home = () => {
                   <p className="mt-4 " id="modal-homepage">
                     Bobby’s Beauty Hall, Sushi Bar, DROOL, South Coast Makers Shop and Bobby’s Ice Cream Parlour will be open from Thursday 9th September.
                   </p>
-                  <p> 
-                    The pop-up Ice Cream Parlour is now closed as we prepare for the unveiling of its permanent location opening this week.
-                  </p>
                   <p className="mt-1 is-size4" id="modal-homepage">
                     GIANT Gallery is now open from 10.00 - 18.00 Monday to Saturday and 10.30 - 16.30 on Sundays.
 
