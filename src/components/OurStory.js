c2684c062832433a4028bef7dbffe01591d97a30
olilewis1 <olilewis1@hotmail.co.uk>
2022-02-02 18:55:45 +0000

    added new navbar items

--- src/components/OurStory.js
+++ src/components/OurStory.js
@@ -26,28 +26,26 @@ const OurStory = () => {
           The property was acquired in late 2019 and Verve Properties who recently transformed The Avenue Centre just up the road are at the helm to bring the Bobby’s building back to life. After the collapse of Debenhams their plans to revive the building and revive The Square in the centre of town were accelerated and plans were quickly in place to re-open the building with a multitude of offerings as soon as possible.
         </p>
         <p className="m-6 is-size-6-touch">
-          Verve Properties are an ethical property firm that regenerates old buildings with the intent to bring communities together and provide an offering for everyone. The company have an ethical and sustainable ethos and focus on working with and securing local, independent, and socially conscious brands and businesses, to ensure that the regeneration reflects the changes Covid has accelerated in social and shopping patterns.
+          Verve Properties are an ethical property firm that regenerates old buildings with the intent to bring communities together and provide an offering for everyone. The company have a sustainable ethos and focus on working with and securing local, independent, and socially conscious brands and businesses, to ensure that the regeneration reflects the changes Covid has accelerated in social and shopping patterns.
         </p>
         <p className="m-6 is-size-6-touch">
-          Building work has been split into a number of phases to ensure the store can open as soon as possible, with phase one opening on 7th August, with more elements opening all the way through the year and into 2022. First to open will be Bobby’s Beauty Hall, Bobby’s Ice Cream Parlour, an art gallery, and Drool - the world’s first food and beverage outlet for dogs, based in the tunnel leading to the Lower Gardens.
+          Building work has been split into a number of phases to ensure the store remains open. With its prominent position in town, it would be disatrous if the lights were turned off until the full renovation is complete. Phase one opened on 9th September 2021, with more elements opening throughout the year and into 2022. First to open was Bobby’s Beauty Hall, Bobby’s Ice Cream Parlour, GIANT art gallery, In Time watch repair and The South Coast Makers Shop.
         </p>
         <p className="m-6 is-size-6-touch">
-          The beauty hall will house local, sustainable, and conscious brands sitting alongside the big global brands we are all familiar with. As an independent beauty operation, Bobby’s Beauty Hall will offer products and brands not previously available at Debenhams, who previously occupied the site. There will also be a Sushi and plant-based Poke Bowl Bar, treatment rooms, female and male grooming. The watch repairing station ‘In Time’ will be retained which is the only watch repair shop nearby.
+          The beauty hall houses local, sustainable, and conscious brands which sit alongside the big global brands we are all familiar with. As an independent beauty operation, Bobby’s Beauty Hall offers products and brands not previously available at Debenhams. The second phase welcomed DRGNFLY Sushi and Champagne bar onto the Ground Floor and The Venue Beuaty Salon.
         </p>
         <p className="m-6 is-size-6-touch">
-          On the first floor the large windows overlooking the Square will be opened up, new balconies installed, and the floor will house a food and market hall which is due to open later this summer. This will include street food outlets including plant-based food, a deli, a bar and speciality retail market pods. Spa facilities are in the planning stages and will hopefully open in 2022.
+          On the first floor the large windows overlooking the Square have been opened up with new balconies installed to match the original balconies on the second floor. This space is currently used for a range of community and local small business supporting events including The South Coast Makers Market which ran for 6 weeks over the Christmas period and will return over Easter this year. The permanent fixture for this floor is due to open in late 2022 and more exciting details will be revealed very soon!
         </p>
-        <p className="m-6 is-size-6-touch">
-        The second floor will house an art gallery with more details to be revealed in the coming weeks. 
-        </p>
+
         <p className="m-6 is-size-6-touch"> 
-        Accessed from Terrace Road, old buildings at the rear will be reorganised to create a garden area and a craft ale smokery bar. Following that, plans are in place to open a fourth floor bar/restaurant and roof terrace bar. – all with a key focus on the local community.
+        Future plans include opening the access to the building from Terrace Road. Old buildings at the rear, including the original bakery will reopen and the space reorganised to create a garden area and a craft ale smokery bar. Following that, plans are in place to open a fourth floor bar/restaurant and roof terrace bar. – all with a key focus on the local community.
         </p>
         <p className="m-6 is-size-6-touch"> 
         Back in 1915, Bobby & Co opened in The Square in Bournemouth and remained under that name for over 50 years, only being rebranded in 1972 as part of the Debenhams group that has now departed the building. The reactions to hearing about the closure of the Debenhams in The Square had caused concern as local people had worried that without the department store that has dominated shopping on The Square and been a focal point of socialisation for over a century, that people would be driven away. There are also concerns about losses of local jobs. 
         </p>
         <p className="m-6 is-size-6-touch"> 
-        And with The Twentieth Century Society reporting in the Observer last month that “landmark UK department stores up and down the country are at risk as Covid changes city centres and that locally beloved buildings, from 1930s classics to brutalist edifices, are facing developers’ wrecking balls” [source] – it is little wonder that people in the area were concerned. Verve hopes that this commitment to restore Bobby’s to its former glory will be welcomed by residents and visitors alike. 
+        And with The Twentieth Century Society reporting in the Observer that “landmark UK department stores up and down the country are at risk as Covid changes city centres and that locally beloved buildings, from 1930s classics to brutalist edifices, are facing developers’ wrecking balls” [source] – it is little wonder that people in the area were concerned. Verve hopes that this commitment to restore Bobby’s to its former glory will be welcomed by residents and visitors alike. 
 
         </p>
         <p className="m-6 is-size-6-touch">
