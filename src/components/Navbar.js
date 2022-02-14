e4330feb8a71b0106675a8251f8f54cef46fbb9d
olilewis1 <olilewis1@hotmail.co.uk>
2022-02-14 13:37:45 +0000

    added new opening items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -95,18 +95,18 @@ const Navbar = () => {
                     </div>
                     <div className="modal-card-body has-text-centered" >
                       <p className="mt-4 " id="modal-homepage">
-                        Bobby’s Beauty Hall, South Coast Makers Shop and DRGNFLY Sushi Bar are open from 10:00AM - 5:00PM Mon-Fri, 10:00AM - 6:00PM Sat and 10:30AM - 4:30PM Sun.
+                        Bobby’s Beauty Hall, South Coast Makers Shop and DRGNFLY Sushi Bar are open from 10:00AM - 6:00PM Mon-Sat and 10:30AM - 4:30PM Sun.
                       </p>
                       <p className="mt-4 " id="modal-homepage">
                         Bobby’s Ice Cream Parlour: 7:30AM - 3:00PM Mon-Fri and 10:00AM - 4:00PM Sat-Sun.
                       </p>
+                      <p className="mt-4 " id="modal-homepage">
+                        The Venue Beauty Salon: 10:00AM - 6:00PM Tues-Sat.
+                      </p>
+                      <p className="mt-4 " id="modal-homepage">
+                        GIANT Gallery: Closed for renovations, back in April.
+                      </p>
 
-                      <p className="mt-4 " id="modal-homepage">
-                        GIANT Gallery: 11:00AM - 5:00PM Mon-Fri, 11:00AM - 6:00PM Sat and 11:00AM - 4:00PM Sun.
-                      </p>
-                      <p className="mt-4 " id="modal-homepage">
-                        The Venue Beauty Salon: 10:00AM - 5:00PM Tues-Sat.
-                      </p>
                       <p className="mt-4 " id="modal-homepage">
                         Dogs are welcome in all areas of the store, except the art gallery.
                       </p>
